48d2423b5c1ec35e6aaafe729a5891cef6caf1d7
Aniello-Bacarella <aniello.bacarella@gmail.com>
2025-08-21 18:44:03 -0400

    finished

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log;
+
+var code1;
+
+code1 = 7 + 3;
+
+var code2 = (100 - 20) / 2;
+
+var code3 = 100 % 61;
+
+var message = "The vault has been secured. The combination is:";
+
+var codeA = code1 + "-" + code2 + "-" + code3;
+
+var codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
